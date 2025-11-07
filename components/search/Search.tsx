876eb58c5fd88d60387295feef62393d18b6241c
Forhad Reza <forhad.bimt@gmail.com>
2025-11-07 22:06:23 +0600

    feat: Enhance search UI and integrate new UI components

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -1,3 +1,16 @@
+import {
+   Select,
+   SelectContent,
+   SelectGroup,
+   SelectItem,
+   SelectLabel,
+   SelectTrigger,
+   SelectValue,
+} from '@/components/ui/select';
+import { Input } from '../ui/input';
+import DatePicker from '../DatePicker';
+import { Label } from '../ui/label';
+import { Button } from '../ui/button';
 const Search = ({ fromList }: { fromList: boolean }) => {
    return (
       <>
@@ -6,9 +19,14 @@ const Search = ({ fromList }: { fromList: boolean }) => {
                id="searchParams"
                className={(fromList && 'shadow-none!') || ''}
             >
-               <div>
-                  <span>Destination</span>
-                  <h4 className="mt-2">
+               <div className="flex flex-col gap-3">
+                  <Label
+                     htmlFor="destination"
+                     className="px-1 font-bold text-md mt-1.5"
+                  >
+                     Destination
+                  </Label>
+                  {/* <h4 className="mt-2">
                      <select name="destination" id="destination">
                         <option value="Bali">Bali</option>
                         <option value="Bali">Coxs Bazar</option>
@@ -16,28 +34,45 @@ const Search = ({ fromList }: { fromList: boolean }) => {
                         <option value="Bali">Saint Martin</option>
                         <option value="Bali">Bali</option>
                      </select>
+                  </h4> */}
+                  
+                  <Select name="destination">
+                     <SelectTrigger className="w-full">
+                        <SelectValue
+                           className="font-normal"
+                           placeholder="Select a destination"
+                        />
+                     </SelectTrigger>
+                     <SelectContent >
+                        <SelectItem value="Bali">Bali</SelectItem>
+                        <SelectItem value="Coxs Bazar">Coxs Bazar</SelectItem>
+                        <SelectItem value="blueberry">sylhet</SelectItem>
+                        <SelectItem value="grapes">Saint Martin</SelectItem>
+                     </SelectContent>
+                  </Select>
+               </div>
+
+               <div>
+                  {/* <span>Check in</span> */}
+                  <h4 className="mt-2">
+                     {/* <Input type="date" name="checkin" id="checkin" /> */}
+                     <DatePicker title={'Check in'} />
                   </h4>
                </div>
 
                <div>
-                  <span>Check in</span>
+                  {/* <span>Checkout</span> */}
                   <h4 className="mt-2">
-                     <input type="date" name="checkin" id="checkin" />
-                  </h4>
-               </div>
-
-               <div>
-                  <span>Checkout</span>
-                  <h4 className="mt-2">
-                     <input type="date" name="checkout" id="checkout" />
+                     {/* <Input type="date" name="checkout" id="checkout" /> */}
+                     <DatePicker title={'Checkout'} />
                   </h4>
                </div>
             </div>
          </div>
 
-         <button className="search-btn">
+         <Button className="search-btn">
             🔍️ {fromList ? 'Modify Search' : 'Search'}
-         </button>
+         </Button>
       </>
    );
 };
