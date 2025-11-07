113d250e180bf1bdca6623fdf9737c01930ed57a
Forhad Reza <forhad.bimt@gmail.com>
2025-11-07 23:52:40 +0600

    Improve ProfileInfo avatar rendering and fallback styling

--- components/user/ProfileInfo.tsx
+++ components/user/ProfileInfo.tsx
@@ -9,10 +9,10 @@ const ProfileInfo = async () => {
   //  console.log(session);
    return (
       <div className="flex flex-col items-center py-8 text-center">
-         <div className="relative max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] bg-orange-600 grid place-items-center text-4xl text-white">
-            <Avatar className='w-full h-auto ring-2 ring-primary ring-offset-2'>
+         <div className="relative max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] grid place-items-center text-4xl text-white">
+            <Avatar className='w-full h-full ring-2 ring-primary ring-offset-2'>
                <AvatarImage src={(session?.user?.image && session?.user?.image) || ''} />
-               <AvatarFallback className="font-semibold">
+               <AvatarFallback className="font-semibold bg-primary">
                   {session?.user?.name[0]}
                </AvatarFallback>
             </Avatar>
