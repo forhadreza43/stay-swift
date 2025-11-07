2defaccd92d3cd26608e56dcd8fd0c758f994336
Forhad Reza <forhad.bimt@gmail.com>
2025-11-08 02:23:43 +0600

    Update Next.js image config for remote hotel images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   /* config options here */
+   images: {
+      remotePatterns: [
+         {
+            hostname: 'a0.muscache.com',
+         },
+      ],
+   },
 };
 
 export default nextConfig;
