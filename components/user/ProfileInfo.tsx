f44943563b29fc9a81352fd97c15d2feab35dc8b
Forhad Reza <forhad.bimt@gmail.com>
2025-11-07 22:06:22 +0600

    feat: Display authenticated user info on Bookings page profile

--- components/user/ProfileInfo.tsx
+++ components/user/ProfileInfo.tsx
@@ -1,18 +1,35 @@
-const ProfileInfo = () => {
-  return (
-    <div className="flex flex-col items-center py-8 text-center">
-      <div className="relative max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] bg-orange-600 grid place-items-center text-4xl text-white">
-        S
-      </div>
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+const ProfileInfo = async () => {
+   const session = await auth.api.getSession({
+      headers: await headers(), // you need to pass the headers object.
+   });
 
-      <div>
-        <h3 className="text-2xl font-semibold lg:text-[28px]">Sumit Saha</h3>
-        <p className="leading-[231%] lg:text-lg">sumitsaha@gmail.com</p>
-      </div>
+  //  console.log(session);
+   return (
+      <div className="flex flex-col items-center py-8 text-center">
+         <div className="relative max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] bg-orange-600 grid place-items-center text-4xl text-white">
+            <Avatar className='w-full h-auto ring-2 ring-primary ring-offset-2'>
+               <AvatarImage src={(session?.user?.image && session?.user?.image) || ''} />
+               <AvatarFallback className="font-semibold">
+                  {session?.user?.name[0]}
+               </AvatarFallback>
+            </Avatar>
+         </div>
 
-      <div className="w-3/4 border-b border-[#a4a4a4] py-6 lg:py-4"></div>
-    </div>
-  );
+         <div>
+            <h3 className="text-2xl font-semibold lg:text-[28px]">
+               {session?.user?.name || ''}
+            </h3>
+            <p className="leading-[231%] lg:text-lg">
+               {session?.user?.email || ''}
+            </p>
+         </div>
+
+         <div className="w-3/4 border-b border-[#a4a4a4] py-6 lg:py-4"></div>
+      </div>
+   );
 };
 
 export default ProfileInfo;
