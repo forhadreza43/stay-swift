a70ee38a909d5ad465c3e0d6070c6d9019b3dade
Forhad Reza <forhad.bimt@gmail.com>
2025-11-10 00:29:48 +0600

    Refactor Gallery component to use dynamic image rendering

--- components/hotel/details/Gallery.tsx
+++ components/hotel/details/Gallery.tsx
@@ -1,20 +1,34 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-const Gallery = ({ gallery }:{gallery: string[]}) => {
-  return (
-    <section className="container">
-      <div className="grid grid-cols-2 imageshowCase gap-3">
-        <Image src={gallery[0]} className="h-[400px] rounded-lg" width={800} height={400} alt="Image - 1" />
+const Gallery = ({ gallery }: { gallery: string[] }) => {
+   const newGallery = [...gallery];
+   newGallery.shift();
+   return (
+      <section className="container">
+         <div className="grid grid-cols-2 imageshowCase gap-3">
+            <Image
+               src={gallery[0]}
+               className="h-[400px] rounded-lg"
+               width={800}
+               height={400}
+               alt="Image - 1"
+            />
 
-        <div className="grid grid-cols-2 grid-rows-2 h-[400px] gap-3">
-          <Image src={gallery[1]} className="h-[200px] w-full object-cover rounded-lg" width={400} height={400} alt="Image - 2" />
-          <Image src={gallery[2]} className="h-[200px] w-full object-cover rounded-lg" width={400} height={400} alt="Image - 3" />
-          <Image src={gallery[3]} className="h-[200px] w-full object-cover rounded-lg" width={400} height={400} alt="Image - 4" />
-          <Image src={gallery[4]} className="h-[200px] w-full object-cover rounded-lg" width={400} height={400} alt="Image - 5" />
-        </div>
-      </div>
-    </section>
-  );
+            <div className="grid grid-cols-2 grid-rows-2 h-[400px] gap-3">
+               {newGallery.map((imgSrc, index) => (
+                  <Image
+                     key={imgSrc}
+                     src={imgSrc}
+                     className="h-[400px] w-full object-cover rounded-lg"
+                     width={400}
+                     height={400}
+                     alt={`Gallery Image - ${index + 2}`}
+                  />
+               ))}
+            </div>
+         </div>
+      </section>
+   );
 };
 
 export default Gallery;
