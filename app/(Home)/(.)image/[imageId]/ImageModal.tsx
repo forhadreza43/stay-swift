1f39418a65ee0350f4ef2be0a1da689f90df13ad
Forhad Reza <forhad.bimt@gmail.com>
2025-11-12 23:17:55 +0600

    feat: update ImageModal styles and remove unused DialogDescription import

--- app/(Home)/(.)image/[imageId]/ImageModal.tsx
+++ app/(Home)/(.)image/[imageId]/ImageModal.tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/navigation';
 import {
    Dialog,
    DialogContent,
-   DialogDescription,
    DialogHeader,
    DialogTitle,
 } from '@/components/ui/dialog';
@@ -26,7 +25,7 @@ export default function ImageModal({ imageUrl, imageId }: ImageModalProps) {
          router.back();
       }
    };
-
+   // console.log('Image Url: ', imageUrl);
    useEffect(() => {
       if (imageUrl) {
          console.log('ImageModal - imageUrl:', imageUrl);
@@ -41,16 +40,16 @@ export default function ImageModal({ imageUrl, imageId }: ImageModalProps) {
 
    return (
       <Dialog open={open} onOpenChange={handleOpenChange}>
-         <DialogContent className=" sm:max-w-[425px] md:max-w-5xl lg:max-w-7xl h-screen ">
+         <DialogContent className=" sm:max-w-[425px] md:max-w-5xl lg:max-w-7xl h-[80vh] ">
             <DialogHeader className="sr-only">
                <DialogTitle>Image {imageId}</DialogTitle>
             </DialogHeader>
-            <div className="relative w-full h-[90vh] min-h-[500px] flex items-center justify-center">
+            <div className="relative w-full  flex items-center justify-center">
                <Image
                   src={imageUrl}
                   alt={`Image ${imageId}`}
                   fill
-                  className="object-contain"
+                  className="object-contain rounded-md"
                   priority
                />
             </div>
