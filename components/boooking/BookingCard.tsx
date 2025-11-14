72d057cb894536f05aa38fba8488fff46e1d333d
Forhad Reza <forhad.bimt@gmail.com>
2025-11-14 23:08:00 +0600

    Refactor BookingCard and BookingCardSkeleton UI layout and structure

--- components/boooking/BookingCard.tsx
+++ components/boooking/BookingCard.tsx
@@ -7,20 +7,25 @@ export default async function BookingCard({ booking }: { booking: Booking }) {
    const duration = getDayDifference(booking?.checkin?.toISOString().split('T')[0], booking?.checkout?.toISOString().split('T')[0]);
    const pricePerNight = (hotel?.highRate - hotel?.lowRate) / 2; 
    return (
-      <div className="flex justify-between items-center ">
-         <div>
-            <h3 className="text-xl font-semibold">{hotel?.name}</h3>
-            <div className="text-sm text-gray-600 my-4">
-               <p>Check In: {booking.checkin.toISOString().split('T')[0]}</p>
-               <p>Check Out: {booking.checkout.toISOString().split('T')[0]}</p>
+      <div className="bg-[#F6F3E9] p-4 rounded-md">
+         <div className="flex justify-between items-center ">
+            <div className='flex flex-col justify-between'>
+               <h3 className="text-xl font-semibold">{hotel?.name}</h3>
+               <div className="text-sm text-gray-600 my-4">
+                  <p>Check In: {booking.checkin.toISOString().split('T')[0]}</p>
+                  <p>
+                     Check Out: {booking.checkout.toISOString().split('T')[0]}
+                  </p>
+               </div>
             </div>
-         </div>
-
-         <div>
-            <h3 className="text-xl font-semibold text-right">${(pricePerNight * duration).toFixed(2)}</h3>
-            <p className="text-sm text-gray-600">
-               ${pricePerNight.toFixed(2)} per night x {duration} days
-            </p>
+            <div>
+               <h3 className="text-xl font-semibold text-right">
+                  ${(pricePerNight * duration).toFixed(2)}
+               </h3>
+               <p className="text-sm text-gray-600">
+                  ${pricePerNight.toFixed(2)} per night x {duration} days
+               </p>
+            </div>
          </div>
       </div>
    );
--- components/skeleton/BookingCardSkeleton.tsx
+++ components/skeleton/BookingCardSkeleton.tsx
@@ -3,6 +3,7 @@ import { Skeleton } from '../ui/skeleton';
 
 export default function BookingCardSkeleton() {
    return (
+      <>
       <div className="bg-gray-300 p-4 rounded-md">
          <div className="flex justify-between items-center ">
             <div>
@@ -18,5 +19,51 @@ export default function BookingCardSkeleton() {
             </div>
          </div>
       </div>
+      <div className="bg-gray-300 p-4 rounded-md">
+         <div className="flex justify-between items-center ">
+            <div>
+               <Skeleton className="h-6 w-48" />
+               <div className="text-sm my-4">
+                  <Skeleton className="h-4 w-32 mb-2" />
+                  <Skeleton className="h-4 w-32" />
+               </div>
+            </div>
+            <div>
+               <Skeleton className="h-6 w-24 mb-2" />
+               <Skeleton className="h-4 w-40" />
+            </div>
+         </div>
+      </div>
+      <div className="bg-gray-300 p-4 rounded-md">
+         <div className="flex justify-between items-center ">
+            <div>
+               <Skeleton className="h-6 w-48" />
+               <div className="text-sm my-4">
+                  <Skeleton className="h-4 w-32 mb-2" />
+                  <Skeleton className="h-4 w-32" />
+               </div>
+            </div>
+            <div>
+               <Skeleton className="h-6 w-24 mb-2" />
+               <Skeleton className="h-4 w-40" />
+            </div>
+         </div>
+      </div>
+      <div className="bg-gray-300 p-4 rounded-md">
+         <div className="flex justify-between items-center ">
+            <div>
+               <Skeleton className="h-6 w-48" />
+               <div className="text-sm my-4">
+                  <Skeleton className="h-4 w-32 mb-2" />
+                  <Skeleton className="h-4 w-32" />
+               </div>
+            </div>
+            <div>
+               <Skeleton className="h-6 w-24 mb-2" />
+               <Skeleton className="h-4 w-40" />
+            </div>
+         </div>
+      </div>
+      </>
    );
 }
