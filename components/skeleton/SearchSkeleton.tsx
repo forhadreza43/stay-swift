8b8e9b3d2f855f82bda4ded8d3ffa1408523fc7f
Forhad Reza <forhad.bimt@gmail.com>
2025-11-14 22:04:31 +0600

    style: Update skeleton backgrounds to use consistent gray shade

--- components/skeleton/SearchSkeleton.tsx
+++ components/skeleton/SearchSkeleton.tsx
@@ -4,7 +4,7 @@ export default function SearchSkeleton() {
    return (
       <>
          <div className="lg:max-h-[250px] mt-6">
-            <div className="bg-gray-200 w-full h-full py-10 lg:py-12 px-6 lg:px-8 rounded-xl  shadow-lg lg:flex items-center *:flex-1 *:px-4 [&>div>span]:mb-3 space-y-5 lg:space-y-0">
+            <div className="bg-gray-300 w-full h-full py-10 lg:py-12 px-6 lg:px-8 rounded-xl  shadow-lg lg:flex items-center *:flex-1 *:px-4 [&>div>span]:mb-3 space-y-5 lg:space-y-0">
                <div className="flex flex-col gap-3">
                   <Skeleton className="h-5 w-32 mb-1.5 rounded-md" />
                   <Skeleton className="h-10 w-full rounded-md" />
